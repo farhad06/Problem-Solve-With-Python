1722a513e0a5d89bb8bf31eb03c768a4472b074e
Farhad Ahamed Biswas <farhadahamed06@gmail.com>
2023-02-22 10:05:47 +0530

    22th feb

--- Array/Find the Missing Number.py
+++ Array/Find the Missing Number.py
@@ -1,10 +1,11 @@
 def missingNumber(arr):
     n=len(arr)
-    total=(n+1)*(n+2)//2 
+    total=((n+1)*(n+2))//2 
     s=0 
     for i in arr:
         s+=i 
     return total-s 
 
 arr = [1, 2, 3, 5,4,7]
+# arr=[3,0,1]
 print("Missing Number is:",missingNumber(arr))        
---
+++ Leetcode Problems Solution/268. Missing Number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n=len(nums)
+        total=(n*(n+1))//2
+        sum=0
+        for i in nums:
+            sum+=i
+        return total-sum    
+
+           

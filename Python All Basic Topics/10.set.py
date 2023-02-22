d64598b61669da69f02c4c3e969f143fc29a0f03
Farhad Ahamed Biswas <farhadahamed06@gmail.com>
2023-02-22 16:04:58 +0530

    update

--- Python All Basic Topics/10.set.py
+++ Python All Basic Topics/10.set.py
@@ -9,3 +9,26 @@ s={1,2,3,4,5}
 print(s)
 s1=set((3,4,5,67,25,45))
 print(s1)
+
+######################### Some Functions ####################################
+
+"""
+difference() ====> Returns a set containing the difference between two or more sets.
+
+difference_update() ====> method removes the items that exist in both sets.
+
+"""
+print("-----------------------------------------------------------------------------")
+"""
+intersection() ===> Returns a set, that is the intersection of two other sets. 
+
+intersection_update() ===> method removes the items that is not present in both sets. 
+
+"""
+print("-----------------------------------------------------------------------------")
+"""
+symmetric_difference() ===> Returns a set with the symmetric differences of two sets. 
+
+symmetric_difference_update() ====> method updates the original set by removing items that are present in both sets, and inserting the other items.
+
+"""
